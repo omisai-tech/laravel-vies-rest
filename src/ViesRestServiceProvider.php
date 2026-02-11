e02a155c4397b90b0e16cfbc451b142f6fdf13f7
Sándor Gergely Bornemisza <bornemisza@omisai.com>
2026-02-11 11:35:27 +0100

    feat: bind ViesClient and ViesConfig in ViesRestServiceProvider

--- src/ViesRestServiceProvider.php
+++ src/ViesRestServiceProvider.php
@@ -7,11 +7,20 @@ namespace Omisai\LaravelViesRest;
 use Illuminate\Support\ServiceProvider;
 use Omisai\LaravelViesRest\Http\LaravelHttpClientFactory;
 use Omisai\ViesRest\Http\HttpClientFactoryInterface;
+use Omisai\ViesRest\ViesClient;
+use Omisai\ViesRest\ViesConfig;
 
 class ViesRestServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
         $this->app->bind(HttpClientFactoryInterface::class, LaravelHttpClientFactory::class);
+
+        $this->app->bind(ViesClient::class, function ($app) {
+            $config = new ViesConfig;
+            $factory = $app->make(HttpClientFactoryInterface::class);
+
+            return new ViesClient($config, $factory);
+        });
     }
 }
